cc16cf0d24e5935b06da30d94e16bfea84bfb1a7
Muryel cabral <muryel_msn@hotmail.com>
2022-06-02 10:36:21 -0300

    fix: archives

--- App copy.ts
+++
@@ -1,8 +0,0 @@
-import React, { FunctionComponent } from 'react';
-import { WebView } from 'react-native-webview';
-
-const LoginWebView: FunctionComponent = () => (
-  <WebView
-    source={{ uri: 'https://mywebapp.com/login' }}
-  />
-);
