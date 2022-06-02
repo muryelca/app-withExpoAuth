cd09d24f67665895c4adc1a20494a38a9dcff07d
Muryel cabral <muryel_msn@hotmail.com>
2022-06-02 11:55:59 -0300

    fix: some issues

--- src/module/Cookies.tsx
+++ src/module/Cookies.tsx
@@ -1,25 +1,40 @@
-import * as React from "react";
-import { Query } from "react-apollo";
-import gql from "graphql-tag";
-import { Text } from "react-native";
+import * as React from 'react';
+import { WebView, WebViewNavigation } from 'react-native-webview';
+import { NativeSyntheticEvent } from 'react-native';
+import { WebViewMessage } from 'react-native-webview/lib/WebViewTypes';
+import CookieManager from '@react-native-community/cookies';
 
-const cookiesQuery = gql`
-  {
-    cookies {
-      id
-      email
-    }
-  }
+const CHECK_COOKIE: string = `
+  ReactNativeWebView.postMessage("Cookie: " + document.cookie);
+  true;
 `;
 
-export class Cookies extends React.PureComponent {
-  render() {
-    return (
-      <Query query={cookiesQuery}>
-        {({ data }) => {
-          return <Text style={{ fontSize: 30 }}>{JSON.stringify(data)}</Text>;
-        }}
-      </Query>
-    );
+const onNavigationStateChange = (navigationState: WebViewNavigation) => {
+  if (webViewRef.current) {
+    webViewRef.current.injectJavaScript(CHECK_COOKIE);
   }
+};
+
+const onMessage = (event: NativeSyntheticEvent<WebViewMessage>) => {
+  const { data } = event.nativeEvent;
+  CookieManager.set('http://besafefrontend.herokuapp.com/', {
+    name: 'myCookie',
+    value: 'myValue',
+    domain: 'some domain',
+    path: '/',
+    version: '1'
+  }).then((done) => {
+    console.log('CookieManager.set =>', done);
+  });
 }
+
+let webViewRef = React.createRef<WebView>();
+const LoginWebView: React.FunctionComponent = () => (
+  <WebView
+    ref={webViewRef}
+    source={{ uri: 'http://besafefrontend.herokuapp.com/' }}
+    onNavigationStateChange={onNavigationStateChange}
+    onMessage={onMessage}
+    sharedCookiesEnabled
+  />
+);
